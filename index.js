c492110219ed91ad2354695b662384d7fb907d32
Ian <ianpmaher@gmail.com>
2024-05-30 21:51:14 -0400

    update rate limiter

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.use(express.static("public"));
 
 // Define the rate limit rule
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 10, // Limit each IP to 100 requests per windowMs
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
     message: "Too many requests from this IP, please try again after 15 minutes.",
 });
 
