82033424674a26e8556d83194ec41d47ea712cf4
Ian Maher <ianpmaher@gmail.com>
2024-08-08 19:25:41 -0400

    set up routes for random history fact and also full history

--- controllers/historyController.js
+++ controllers/historyController.js
@@ -1,20 +1,30 @@
 const { getHistoryData } = require('../scraping/historyData');
 
+const getFullHistory = (req, res) => {
+    const rhodeIslandData = getHistoryData();
+    res.json(rhodeIslandData);
+};
+
 const getHistoryFactsIndex = () => {
     const rhodeIslandData = getHistoryData();
-    const facts = rhodeIslandData;
-    const randomIndex = Math.floor(Math.random() * facts.length);
+    const randomIndex = Math.floor(Math.random() * rhodeIslandData.length);
     return randomIndex;
 };
 
 const getRandomHistory = (req, res) => {
     const rhodeIslandData = getHistoryData();
-    const facts = rhodeIslandData.facts;
+    const facts = rhodeIslandData;
     const randomIndex = getHistoryFactsIndex();
     const randomFact = facts[randomIndex];
-    res.json({ fact: randomFact });
+    res.json({ 
+        year: randomFact.year,
+        event: randomFact.text,
+        media: randomFact.media,
+        mediaThumbnail: randomFact.mediaThumbnail
+    });
 };
 
 module.exports = {
-    getRandomHistory
+    getRandomHistory,
+    getFullHistory
 };
--- routes/historyRouter.js
+++ routes/historyRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const historyController = require("../controllers/historyController");
 
-router.get("/", historyController.getRandomHistory)
+router.get("/", historyController.getFullHistory)
+router.get("/random", historyController.getRandomHistory)
 
 module.exports = router;
